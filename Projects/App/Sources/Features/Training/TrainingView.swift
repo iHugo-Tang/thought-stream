80fb31527f34f133a8ff141544ca7983e1a70bd3
Hugo <cythbgy@gmail.com>
2025-11-25 10:09:57 -0700

    feat(training): improve text input height and keyboard avoidance for submit button

--- Projects/App/Sources/Features/Training/TrainingView.swift
+++ Projects/App/Sources/Features/Training/TrainingView.swift
@@ -1,9 +1,44 @@
 import SwiftUI
 
 struct TrainingView: View {
+    @State private var text = ""
 
     var body: some View {
-        Text("ProfileView")
+        VStack(spacing: 0) {
+            ScrollView {
+                VStack(spacing: 16) {
+                    VStack(alignment: .center, spacing: 8) {
+                        Text("The limits of my language \nmean the limits of my world.")
+                            .multilineTextAlignment(.center)
+                            .font(FontSize.headline.font())
+                            .foregroundColor(ThoughtStreamAsset.Colors.textPrimary.swiftUIColor)
+                        Text("- Ludwig Wittgenstein")
+                            .font(FontSize.bodyEmphasis.font())
+                            .foregroundColor(ThoughtStreamAsset.Colors.textSecondary.swiftUIColor)
+                    }
+                    
+                    TextField(
+                        "Enter your answer",
+                        text: $text,
+                        prompt: Text("Enter your answer").foregroundColor(ThoughtStreamAsset.Colors.textPlaceholder.swiftUIColor),
+                        axis: .vertical
+                    )
+                    .frame(minHeight: 120, alignment: .top)
+                    .padding()
+                    .background(ThoughtStreamAsset.Colors.bgSecondary.swiftUIColor)
+                    .cornerRadius(CornerSize.small)
+                }
+                .padding()
+            }
+            
+            VStack {
+                LargeButton(buttonType: .primary, title: "Submit") {
+                    
+                }
+            }
+            .padding()
+        }
+        .background(ThoughtStreamAsset.Colors.bgPrimary.swiftUIColor)
     }
 }
 
